4a6c857638de702a8651715f25721347bf77aad1
Lars Ericson <lars.ericson@catskillsresearch.com>
2020-12-10 14:49:13 -0500

    Steinhaus

--- Lecture1.lean
+++ Lecture1.lean
@@ -1,11 +1,10 @@
 import measure_theory.lebesgue_measure
-import measure_theory.borel_space
-import data.set.intervals.basic
-import measure_theory.measure_space
 
 open measure_theory
 
-noncomputable instance {α} {p : α → Prop} [m : measure_space α] : measure_space (subtype p) :=
+noncomputable theory
+
+instance {α} {p : α → Prop} [m : measure_space α] : measure_space (subtype p) :=
 { volume := measure.comap (coe : _ → α) volume }
 
 theorem subtype.volume_apply {α} {p : α → Prop} [measure_space α]
@@ -15,9 +14,17 @@ measure.comap_apply _ subtype.coe_injective (λ _, is_measurable.subtype_image h
 
 abbreviation I01 := (set.Icc (0 : ℝ) 1)
 
-instance : probability_measure (volume : measure I01) :=
+instance P_I01 : probability_measure (volume : measure I01) :=
 { measure_univ := begin
     refine (subtype.volume_apply is_measurable_Icc is_measurable.univ).trans _,
     suffices : volume I01 = 1, {simpa},
     rw [real.volume_Icc], simp
   end }
+
+class probability_space (α : Type*) extends measure_space α :=
+(is_probability_measure:  probability_measure volume)
+
+instance Steinhaus : probability_space I01 := 
+{ is_probability_measure := P_I01 }
+
+#check Steinhaus -- Steinhaus : probability_space ↥I01
--- src/ProbabilitySpace.lean
+++ src/ProbabilitySpace.lean
@@ -1,9 +1,28 @@
--- Let(X, Σ, P) be a probability space
--- where (X, Σ) is a measure space
--- and P: (X, Σ) → [0,1] is a probability measure
+import measure_theory.lebesgue_measure
+open measure_theory
+noncomputable theory
 
-structure is_probability_space {X: Type *} (ΩA: measure_space X) (P: probability_measure ΩA) := ()
+instance {α} {p : α → Prop} [m : measure_space α] : measure_space (subtype p) :=
+{ volume := measure.comap (coe : _ → α) volume }
 
-def is_probability_space.sample_space {X: Type*} {A : set X → Prop} (XA : is_σ_algebra X A):= X
-def is_probability_space.event_space {X: Type*} {A : set X → Prop} (XA : is_σ_algebra X A) := A
-def is_probability_space.probability_measure {X: Type*} {A : set X → Prop} (XA : is_σ_algebra X A) := A
+theorem subtype.volume_apply {α} {p : α → Prop} [measure_space α]
+  (hp : is_measurable {x | p x}) {s : set (subtype p)} (hs : is_measurable s) :
+  volume s = volume ((coe : _ → α) '' s) :=
+measure.comap_apply _ subtype.coe_injective (λ _, is_measurable.subtype_image hp) _ hs
+
+abbreviation I01 := (set.Icc (0 : ℝ) 1)
+
+instance P_I01 : probability_measure (volume : measure I01) :=
+{ measure_univ := begin
+    refine (subtype.volume_apply is_measurable_Icc is_measurable.univ).trans _,
+    suffices : volume I01 = 1, {simpa},
+    rw [real.volume_Icc], simp
+  end }
+
+class probability_space (α : Type*) extends measure_space α :=
+(is_probability_measure:  probability_measure volume)
+
+instance Steinhaus : probability_space I01 := 
+{ is_probability_measure := P_I01 }
+
+#check Steinhaus -- Steinhaus : probability_space ↥I01
